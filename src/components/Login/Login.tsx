ec85acd254c3ba07abf50eff56edc412bec060f6
shubham-jaishu <shubhamjaishu@gmail.com>
2024-09-25 10:13:53 +0530

    handle login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -12,24 +12,21 @@ const Login = () => {
 
     const router = useRouter()
 
-    const handleLogin = () => {
-        console.log("login");
-        router.push("/")
-    }
-
     return (
         <div className="wrapper w-full h-screen bg-white">
-            <div className="content h-[410px] flex flex-col justify-between">
+            <div className="content h-[420px] flex flex-col justify-between">
+                
                 <h3 className='pt-4'>Login Your Account</h3>
-                <form className="form h-4/5 flex flex-col mt-2" action="/login" method="post">
+                
+                <form className="form h-4/5 flex flex-col mt-2" action="/" method="post">
                     <input type="email" placeholder="Email" name="email" required className='mt-4 h-10' />
                     <input type="password" placeholder="Password" name="password" required className='h-10' />
-                    <input type="submit" value="Login" className='mt-2' onClick={() => {handleLogin()}}/>
-                    <div >
-                        <button className='flex justify-center items-center w-full gap-x-2 bg-white hover:bg-slate-100 rounded-md p-2 mt-4' onClick={() => {signInWithGoogle(router)}}><Image src={assets.google} alt='Google_icon' id='icon'></Image>Login with Google</button>
-                    </div>
-                    <Link href="/signup" className='mt-2'>Create new account</Link>
+                    <input type="submit" value="Login" className='mt-2' />
                 </form>
+                <div >
+                    <button className='flex justify-center items-center w-full gap-x-2 bg-white hover:bg-slate-100 rounded-md p-2 mt-4' onClick={() => { signInWithGoogle(router) }}><Image src={assets.google} alt='Google_icon' id='icon'></Image>Login with Google</button>
+                </div>
+                <Link href="/signup" className='mt-3'>Create new account</Link>
             </div>
         </div>
     );
--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -8,32 +8,26 @@ import { assets } from '@/assets/assets';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-
 const Signup = () => {
 
   const router = useRouter()
-
-  const handleLogin = () => {
-    console.log("login");
-    router.push("/")
-}
 
   return (
     <div className="wrapper w-full h-screen bg-white">
       <div className="content h-[570px] flex flex-col justify-between">
         <h3 className='mt-3'>Create Account</h3>
-        <form className="form text-black h-[470px]" action="/register" method="post">
+        <form className="form text-black h-[470px]" action="/" method="post">
           <input type="text" placeholder="Username" name="username" required className='mt-2' />
           <input type="text" placeholder="Name" name="name" required />
           <input type="number" placeholder="Age" name="age" required />
           <input type="email" placeholder="Email" name="email" required />
           <input type="password" placeholder="Password" name="password" required />
-          <input type="submit" value="Create" className='mt-3' onClick={() => handleLogin()} />
-          <div >
-            <button className='flex justify-center items-center w-full gap-x-2 bg-white hover:bg-slate-100 rounded-md p-2 mt-4' onClick={() => {signInWithGoogle(router)}}><Image src={assets.google} alt='Google_icon' id='icon'></Image>Signin with Google</button>
-          </div>
-          <Link href="/login" className='mt-3'>Already have account</Link>
+          <input type="submit" value="Create" className='mt-3' />
         </form>
+        <div >
+          <button className='flex justify-center items-center w-full gap-x-2 bg-white hover:bg-slate-100 rounded-md p-2 mt-4' onClick={() => { signInWithGoogle(router) }}><Image src={assets.google} alt='Google_icon' id='icon'></Image>Signin with Google</button>
+        </div>
+        <Link href="/login" className='mt-3'>Already have account</Link>
       </div>
     </div>
   );
